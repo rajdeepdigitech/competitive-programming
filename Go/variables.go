2b7da4447ee1451817c342a01cd21505498185b8
Rajdeep Digitech <rajdeepdigitech@gmail.com>
2026-07-02 11:03:50 +0530

    Create variables.go

---
+++ Go/variables.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// var student1 string = "John" // type is string
+	// var student2 = "Jane"        // type is inferred
+	// x := 2                       // type is inferred
+
+	// 	fmt.Println(student1)
+	// 	fmt.Println(student2)
+	// 	fmt.Println(x)
+
+	var a string
+	var b int
+	var c bool
+
+	var x, y, z, u, v, w int = 1, 2, 3, 4, 5, 6
+
+	fmt.Println(u)
+	fmt.Println(v)
+	fmt.Println(w)
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(z)
+
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+
+}
